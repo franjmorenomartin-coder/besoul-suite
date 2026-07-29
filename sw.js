836399dec070f70d94da80683895e8fe92f5ee6d
franjmorenomartin-coder <fran.jmorenomartin@gmail.com>
2026-07-29 08:58:43 +0200

    Corregir logo app móvil BESOUL

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'besoul-suite-v1';
+const CACHE_NAME = 'besoul-suite-v2';
 const CORE_ASSETS = [
   './',
   './index.html',
@@ -8,23 +8,38 @@ const CORE_ASSETS = [
   './dashboard.html',
   './reservas.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './besoul-icon-192.png',
+  './besoul-icon-512.png',
+  './apple-touch-icon.png'
 ];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).catch(() => null));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CORE_ASSETS))
+      .catch(() => null)
+  );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', event => {
   const req = event.request;
   if (req.method !== 'GET') return;
-  event.respondWith(fetch(req).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => null);
-    return res;
-  }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html'))));
+
+  event.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => null);
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html')))
+  );
 });
