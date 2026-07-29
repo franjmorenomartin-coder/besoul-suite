2ad82038721421dba66b3a7a46a4c446d90cdaf5
franjmorenomartin-coder <fran.jmorenomartin@gmail.com>
2026-07-29 08:40:20 +0200

    Create sw.js
    Añadir service worker BESOUL

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'besoul-suite-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './agenda.html',
+  './crm.html',
+  './finanzas.html',
+  './dashboard.html',
+  './reservas.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).catch(() => null));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => null);
+    return res;
+  }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html'))));
+});
